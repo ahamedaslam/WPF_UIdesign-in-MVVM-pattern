9dd3ecae2d3a89bc5237c73344c867674f6d9821
ahamedaslam <87639294+ahamedaslam@users.noreply.github.com>
2023-09-23 14:55:12 +0530

    Add project files.

---
+++ WPF_UIdesign/ViewModel/ViewModelCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_UIdesign.ViewModel
+{
+    public class ViewModelCommand: ICommand
+    {
+        //Fields
+        private readonly Action<object> _executeAction;
+        private readonly Action<object> _canexecuteAction;
+
+        //Constructors
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+            _canexecuteAction = null;
+        }
+
+        public ViewModelCommand(Action<object> executeAction, Action<object> canexecuteAction)
+        {
+            _executeAction = executeAction;
+            _canexecuteAction = canexecuteAction;
+        }
+
+
+        
+        //Event
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
